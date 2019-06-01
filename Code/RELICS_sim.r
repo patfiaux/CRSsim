4858e01920f22a3faac1abd8a194cae9779d8462
patfiaux <pfiaux@eng.ucsd.edu>
2019-05-31 18:37:57 -0700

    updated RELICS_sim.r
    Added default parameters to make usage easier

--- Code/RELICS_sim.r
+++ Code/RELICS_sim.r
@@ -69,7 +69,214 @@ experiment_sequencing <- function(input.pools, pcr.type, input.seq.depth){
   return(out.df)
 }
 
-simulate_data <- function(input.list, two.distr = NULL, using.facs = 'yes'){
+set_default_flags <- function(input.list){
+
+  out.list <- input.list
+  input.list.names <- names(input.list)
+
+  if(! 'outDir' %in% input.list.names){
+    out.list$outDir <- ''
+  }
+  if(! 'randomizeRepl' %in% input.list.names){
+    # in case the correlation between replicates is to be broken
+    out.list$randomizeRepl <- 'no'
+  }
+  if(! 'crisprEffectRange' %in% input.list.names){
+    if(out.list$crisprSystem == 'CRISPRi' | out.list$crisprSystem == 'CRISPRa'){
+      out.list$crisprEffectRange <- 1000
+    } else if(out.list$crisprSystem == 'Cas9'){
+      out.list$crisprEffectRange <- 20
+    } else if(out.listout.list$crisprSystem == 'dualCRISPR'){
+      out.list$crisprEffectRange <- 0
+    } else {
+      print('Error: please specify a valid CRISPR system (Cas9, CRISPRi, CRISPRa, dualCRISPR)')
+      break()
+    }
+  }
+  if(! 'nrNeg' %in% input.list.names){
+    out.list$nrNeg <- 300
+  }
+  if(! 'nrSims' %in% input.list.names){
+    out.list$nrSims <- 1
+  }
+  if(! 'inputCellNr' %in% input.list.names){
+    out.list$inputCellNr <- rep(50e6, ncol(out.list$inputGuideDistr))
+  }
+  if('enhancerStrenth' %in% input.list.names){
+    if(out.list$enhancerStrenth == 'high'){
+      out.list$enhancerShape1 <- 7
+      out.list$enhancerShape2 <- 2
+    } else if(out.list$enhancerStrenth == 'medium'){
+      out.list$enhancerShape1 <- 5
+      out.list$enhancerShape2 <- 5
+    } else if(out.list$enhancerStrenth == 'low'){
+      out.list$enhancerShape1 <- 2
+      out.list$enhancerShape2 <- 7
+    } else{
+      print('Error: please specify valid enhancerStrenth (high, medium, low)')
+      break()
+    }
+  }
+  if('guideEfficiency' %in% input.list.names){
+    if(out.list$guideEfficiency == 'high'){
+      out.list$guideShape1 <- 7
+      out.list$guideShape2 <- 2
+    } else if(out.list$guideEfficiency == 'medium'){
+      out.list$guideShape1 <- 5
+      out.list$guideShape2 <- 5
+    } else if(out.list$guideEfficiency == 'low'){
+      out.list$guideShape1 <- 2
+      out.list$guideShape2 <- 7
+    } else{
+      print('Error: please specify valid guideEfficiency (high, medium, low)')
+      break()
+    }
+  }
+  if('selectionScreen' %in% input.list.names){
+    if(out.list$selectionScreen != 'yes'){
+      print('Specified FACS screen')
+      out.list$FACSscreen <- 'yes'
+      out.list$selectionScreen <- NULL
+    } else {
+
+      # add the second sequencing depth parameter
+      temp.seq.depth <- out.list$seqDepth
+
+      adj.seq.depth <- lapply(temp.seq.depth, function(x){
+        return(c(x, x[length(x)]))
+      })
+
+      out.list$seqDepth <- adj.seq.depth
+
+      if('selectionStrength' %in% input.list.names){
+        if(out.list$selectionStrength == 'high'){
+          out.list$posSortingFrequency <- c(1, 95)
+          out.list$negSortingFrequency <- c(5, 95)
+        } else if(out.list$selectionStrength == 'low'){
+          out.list$posSortingFrequency <- c(4, 95)
+          out.list$negSortingFrequency <- c(5, 95)
+        } else {
+          print('Specified selectionStrength not yet implemented. Pick either high or low or specify parameters manually')
+          break()
+        }
+      } else {
+        if(! 'posSortingFrequency' %in% input.list.names){
+          print('strength of selectionScreen has not been specified. Either set selectionStrength to high, low or specify the parameters manually')
+          break()
+        } else if(length(out.list$posSortingFrequency) == 1){
+          out.list$posSortingFrequency <- c(out.list$posSortingFrequency, 95)
+          out.list$negSortingFrequency <- c(out.list$negSortingFrequency, 95)
+        }
+      }
+    }
+  }
+  if('FACSscreen' %in% input.list.names){
+    if('selectionScreen' %in% names(out.list)){
+      print('Conflicting info about screen type. Pick either selectionScreen or FACSscreen')
+      break()
+    } else {
+      if('selectionStrength' %in% input.list.names){
+        if(out.list$selectionStrength == 'high'){
+          out.list$posSortingFrequency <- c(rep(97, (nrow(out.list$seqDepth) - 1) ), 13)*0.5
+          out.list$negSortingFrequency <- c(rep(97, (nrow(out.list$seqDepth) - 1) ), 3)*0.5
+        } else if(out.list$selectionStrength == 'high'){
+          out.list$posSortingFrequency <- c(rep(97, (nrow(out.list$seqDepth) - 1) ), 5)*0.5
+          out.list$negSortingFrequency <- c(rep(97, (nrow(out.list$seqDepth) - 1) ), 3)*0.5
+        } else {
+          print('Specified selectionStrength not yet implemented. Pick either high or low or specify parameters manually')
+        }
+      } else if(! 'posSortingFrequency' %in% names(out.list)){
+        print('strength of selectionScreen has not been specified. Either set selectionStrength to high, low or specify the parameters manually')
+      }
+    }
+  }
+
+  if(! 'selectionScreen' %in% names(out.list) | 'FACSscreen' %in% names(out.list)){
+    print('No info about screen type. Pick either selectionScreen or FACSscreen')
+    break()
+  }
+
+  if(! 'enhancerShape1' %in% names(out.list)){
+    print('Error: please specify valid enhancerStrenth (high, medium, low) or set the parameters for enhancerShape1 manually')
+    break()
+  }
+  if(! 'enhancerShape2' %in% names(out.list)){
+    print('Error: please specify valid enhancerStrenth (high, medium, low) or set the parameters for enhancerShape2 manually')
+    break()
+  }
+  if(! 'guideShape1' %in% names(out.list)){
+    print('Error: please specify valid guideEfficiency (high, medium, low) or set the parameters for guideShape1 manually')
+    break()
+  }
+  if(! 'guideShape2' %in% names(out.list)){
+    print('Error: please specify valid guideEfficiency (high, medium, low) or set the parameters for guideShape2 manually')
+    break()
+  }
+
+  return(out.list)
+}
+
+simulate_data <- function(input.list){
+
+  input.list <- set_default_flags(input.list)
+
+  updated.info <- input.list$guides  #input.info # need to update
+
+  if(input.list$crisprSystem %in% c('CRISPRi', 'CRISPRa', 'CRISPRcas9')){
+    updated.info$start <- updated.info$start - round(input.list$crisprEffectRange / 2)
+    updated.info$end <- updated.info$end + round(input.list$crisprEffectRange / 2)
+  }
+
+  dir.create(file.path(paste0(input.list$outDir, input.list$simName)))
+
+  for(sim in 1:input.list$nrSims){
+    print(paste0('Starting simulation ', sim, ' out of ', input.list$nrSims))
+    input.list$inputPools <- list()
+    input.list$enhancer <- generate_enhancers(input.list, updated.info, input.list$exon)
+
+    print('Generating initial pool')
+    for(repl in 1:length(input.list$inputCellNr)){
+      if(input.list$randomizeRepl == 'yes'){
+        randomized.counts <- sample(input.list$inputGuideDistr[, repl])
+        input.list$inputPools[[paste0('repl',repl)]] <- rmultinom(1,
+          input.list$inputCellNr[repl], randomized.counts)
+      } else {
+        input.list$inputPools[[paste0('repl',repl)]] <- rmultinom(1,
+          input.list$inputCellNr[repl], input.list$inputGuideDistr[, repl])
+      }
+    }
+
+
+    temp.sim <- full_replicate_simulation_sepDistrSampl(input.list, updated.info, sim)
+
+    write.csv(temp.sim$counts, file = paste0(input.list$outDir, input.list$simName,'/',
+      input.list$simName, '_sim', sim, '_counts.csv'), row.names = F)
+
+    input.list$enhancer$enhancerStrength <- temp.sim$enhancerStrength
+    ordered.enhancers <- input.list$enhancer[order(input.list$enhancer$start),]
+
+    write.csv(ordered.enhancers, file = paste0(input.list$outDir, input.list$simName,'/',
+      input.list$simName, '_sim', sim, '_enhancers.csv'), row.names = F)
+
+    init.labels <- rep('chr', nrow(updated.info))
+    final.info <- input.list$guides
+    final.info$label <- init.labels
+    exon.label.pos <- sim_enhancer_overlaps(updated.info, input.list$exon) # also works for exons
+    final.info$label[exon.label.pos] <- 'exon'
+    enh.label.pos <- sim_enhancer_overlaps(updated.info, ordered.enhancers)
+    final.info$label[enh.label.pos] <- 'pos'
+    random.neg <- sample(which(final.info$label == 'chr'), input.list$nrNeg)
+    final.info[random.neg, c(1:4)] <- data.frame(NA, NA, NA, 'neg', stringsAsFactors = F)
+    write.csv(final.info, file = paste0(input.list$outDir, input.list$simName,'/',
+      input.list$simName, '_sim', sim, '_info.csv'), row.names = F)
+  }
+}
+
+# below was discontinued on 19/05/31 and re-written to simplify the process of data simulation
+# this does change a few things, especially with the input and therefore is not fully backward compatible
+simulate_data_discontinued <- function(input.list, two.distr = NULL, using.facs = 'yes'){
+
+  input.list <- set_default_flags(input.list)
 
   updated.info <- input.list$guides  #input.info # need to update
 
@@ -158,6 +365,7 @@ full_replicate_simulation_sepDistrSampl <- function(input.frame, input.info, sim
 
   # for the number of replicates
   for(i in 1:length(input.frame$inputPools)){
+    print(paste0('Generating replicate ', i))
     # add counts, assuming negative sorting probabilities
     temp.sort.prob <- rdirichlet(length(input.frame$inputPools[[i]]),
       input.frame$negSortingFrequency)
@@ -179,7 +387,7 @@ full_replicate_simulation_sepDistrSampl <- function(input.frame, input.info, sim
 
       temp.exon.sort.prob <- c()
 
-      # since dealing with exons, there is no exon efficiency
+      # since dealing with exons, there is no guide efficiency
       # for each guide, the efficient guide gets sorted according to exon efficiency
       # the remaining guides get sorted according to negative sorting frequencies
       for(g in 1:length(unique.guide.overlaps)){
@@ -281,14 +489,21 @@ full_replicate_simulation_sepDistrSampl <- function(input.frame, input.info, sim
     }
     repl.sequenced <- experiment_sequencing(cbind(input.frame$inputPools[[i]], repl.counts),
       input.frame$pcrDupl, input.frame$seqDepth[[i]])
-    colnames(repl.sequenced) <- paste(paste0('sim', sim.nr, '_repl', i), input.frame$poolNames, sep = '_')
-    out.sim.data <- cbind(out.sim.data, repl.sequenced)
+
+    repl.sequenced.filtered <- repl.sequenced
+    if('selectionScreen' %in% names(input.frame)){
+      repl.sequenced.filtered <- repl.sequenced[,c(1,2)]
+    }
+
+    colnames(repl.sequenced.filtered) <- paste(paste0('sim', sim.nr, '_repl', i), input.frame$poolNames, sep = '_')
+    out.sim.data <- cbind(out.sim.data, repl.sequenced.filtered)
   }
   return(list(counts = out.sim.data, enhancerStrength = sort.factor))
 }
 
 # give different genomic properties different sorting probabilities
 # this method creates a compound distribution for obtinain the sorting probabilities
+# deprecated
 full_replicate_simulation <- function(input.frame, input.info, sim.nr){
 
   out.sim.data <- c()
